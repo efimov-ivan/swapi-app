853fd1c0f9a31ca80ec815abcf2607965c810a44
I.Efimov <i.efimov.jamteam@gmail.com>
2020-11-07 01:28:49 +0600

    Updates

---
+++ precache-manifest.9d7e5cdf7807076d70dcc9dc092bd7c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b23f6cc6d9a9dbec5c6b593d889585a",
+    "url": "/swapi-app/index.html"
+  },
+  {
+    "revision": "4d9d23c482ccf2b2b299",
+    "url": "/swapi-app/static/css/main.b2549d25.chunk.css"
+  },
+  {
+    "revision": "f844316c262e70086ce3",
+    "url": "/swapi-app/static/js/2.12d6ed56.chunk.js"
+  },
+  {
+    "revision": "4d9d23c482ccf2b2b299",
+    "url": "/swapi-app/static/js/main.b4dfb612.chunk.js"
+  },
+  {
+    "revision": "80d828082ec61527edcd",
+    "url": "/swapi-app/static/js/runtime-main.92052cbb.js"
+  },
+  {
+    "revision": "f6acb1cb21fc5b2b539068590df44338",
+    "url": "/swapi-app/static/media/space.f6acb1cb.jpg"
+  }
+]);
